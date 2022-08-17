32a04b0692c96f356aab07dd0794f7bf830cc71a
Yared S <s.yared25@gmail.com>
2022-08-17 09:29:51 +0300

    Mandatory task update

---
+++ 102-counting_sort.c
@@ -0,0 +1,9 @@
+#include "sort.h"
+#include <stdio.h>
+
+void counting_sort(int *array, size_t size)
+{
+	printf("Not Implemented.\n");
+	array++;
+	size++;
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Sorting algorithm and Bir O
+Mandatory task 100 and 101 included
